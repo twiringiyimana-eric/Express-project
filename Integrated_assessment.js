dd6d225dd393a595c508076375c9fc4ee11117db
developmentsoftware24 <softwaredevelopment24@gmail.com>
2025-02-12 18:45:18 +0200

    added

---
+++ Integrated_assessment.js
@@ -0,0 +1,13 @@
+// create  node js application that will perform the following:
+// 1. Create a server that listens on port 8081
+// 2. Create a connection to the database
+// 3. Create a table in the database
+// 4. Insert data into the table
+// 5. Retrieve data from the table
+// 6. Update data in the table
+// 7. Delete data from the table
+// 8. Close the connection to the database
+// database name should be integrated_assessment
+// and the table name should be users
+
+                            //  happy coding
